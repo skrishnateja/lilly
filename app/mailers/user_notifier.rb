bd020cee7377b29a4f96d9a1d231d508f08b19f5
unknown <s_krishnateja@yahoo.in>
2015-08-09 08:18:23 +0530

    look

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -4,15 +4,15 @@ class UserNotifier < ApplicationMailer
 		@user = user_friendship.user
 		@friend = user_friendship.friend
 
-		mail to: @friend.email
-		subject: "#{@user.first_name} sent you a request"
+		mail( to: @friend.email ,
+		subject: "{@user.first_name} sent you a request")
 	end
 	def friend_requested(user_friendship_id)
 		user_friendship UserFriendship.find(user_friendship_id)
 		@user = user_friendship.user
 		@friend = user_friendship.friend
 
-		mail to: @user.email
-		subject: "#{@friend.first_name} accepted you a request"
+		mail (to: @user.email ,
+		subject: "{@friend.first_name} accepted you a request")
 	end
 end
